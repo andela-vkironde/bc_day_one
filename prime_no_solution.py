29b9561af616cda33b1227380245341548dc7b34
WNjihia <dorisn172@gmail.com>
2017-03-07 01:38:09 +0300

    add test to check odd number in prime number list

--- prime_no_solution.py
+++ prime_no_solution.py
@@ -1,12 +1,17 @@
 def gen_prime_number(n):
+	list_of_prime_numbers = []
 	if n is not None:
-	    if n > 0:
-	    	for value in range (0,n):
-	    		if value % 2 == 0:
-	    			print (value)
-	    		else:
-	    			pass
-	    else:
-	    	return("Error: Input positive number")
+		if n > 0:
+		    for value in range (0,n):
+		    	if value % 2 == 0:
+		    		list_of_prime_numbers.append(value)
+		    	else:
+		    			pass
+		    return (list_of_prime_numbers)
+		else:
+		    return("Error: Input positive number")
 	else:
 		return("Error: Invalid input")
+
+
+gen_prime_number(10)
--- test_solution.py
+++ test_solution.py
@@ -1,18 +1,22 @@
 import unittest
 from prime_no_solution import gen_prime_number
+from testtools import ExpectedException
+# from import org.junit.rules.ExpectedException
 
 class Solution(unittest.TestCase):
 	def test_n_is_seven(self):
-		value = gen_prime_number(7)
-		self.assertEqual(gen_prime_number(7), value)
+		value = gen_prime_number(8)
+		self.assertEqual(gen_prime_number(8), value)
 
 	def test_negative_n (self):
 		self.assertEqual(gen_prime_number(-4),"Error: Input positive number")
 
-	def test_n_not_null (self):
-	    n = None 
-	    self.assertEqual(gen_prime_number(n),"Error: Invalid input")
- #        assert n Is Not None
+	def test_odd_number (self):
+		container = gen_prime_number(10)
+		self.assertNotIn(5, container)
+
+	def test_n_null (self):
+		self.assertEqual(gen_prime_number(None),"Error: Invalid input")
 
 if __name__ == "__main__":
     unittest.main()
