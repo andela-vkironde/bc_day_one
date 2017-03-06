09f5b92a8d937199ea8bf111f564a636e077792a
WNjihia <dorisn172@gmail.com>
2017-03-07 01:40:38 +0300

    add test to check type of output

--- test_solution.py
+++ test_solution.py
@@ -18,5 +18,8 @@ class Solution(unittest.TestCase):
 	def test_n_null (self):
 		self.assertEqual(gen_prime_number(None),"Error: Invalid input")
 
+	def test_if_list (self):
+		assert type(gen_prime_number(10)) is list
+
 if __name__ == "__main__":
     unittest.main()
