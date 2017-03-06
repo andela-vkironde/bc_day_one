270731bf499e139a1df943851543b52fc92bd37f
WNjihia <dorisn172@gmail.com>
2017-03-06 23:16:53 +0300

    add test to check for not none

--- prime_no_solution.py
+++ prime_no_solution.py
@@ -1,9 +1,12 @@
 def gen_prime_number(n):
-	if n > 0:
-		for value in range (0,n):
-			if value % 2 == 0:
-				print (value)
-			else:
-				pass
+	if n is not None:
+	    if n > 0:
+	    	for value in range (0,n):
+	    		if value % 2 == 0:
+	    			print (value)
+	    		else:
+	    			pass
+	    else:
+	    	return("Error: Input positive number")
 	else:
-		return("Error: Input positive number")
+		return("Error: Invalid input")
--- test_solution.py
+++ test_solution.py
@@ -6,8 +6,13 @@ class Solution(unittest.TestCase):
 		value = gen_prime_number(7)
 		self.assertEqual(gen_prime_number(7), value)
 
-	def test_negative_n (self): 
+	def test_negative_n (self):
 		self.assertEqual(gen_prime_number(-4),"Error: Input positive number")
+
+	def test_n_not_null (self):
+	    n = None 
+	    self.assertEqual(gen_prime_number(n),"Error: Invalid input")
+ #        assert n Is Not None
 
 if __name__ == "__main__":
     unittest.main()
